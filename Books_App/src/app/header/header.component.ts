9f92071ede4ca48673a2a2c8dff27ff7874e88b1
enzojack <lc.mihut@gmail.com>
2019-02-26 22:46:42 +0200

    Started working at adding new modules in application!!

--- Books_App/src/app/header/header.component.ts
+++ Books_App/src/app/header/header.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
 import { Response } from '@angular/http';
+import { AuthService } from '../auth/auth.service';
 
 @Component({
     selector: 'app-header',
@@ -9,7 +10,7 @@ import { Response } from '@angular/http';
 
 export class HeaderComponent {
 
-    constructor(private dataStorageService: DataStorageService) {}
+    constructor(private dataStorageService: DataStorageService, public authService: AuthService) {}
 
     onSaveData() {
         this.dataStorageService.storeRecipes()
@@ -22,5 +23,9 @@ export class HeaderComponent {
 
     onFetchData() {
         this.dataStorageService.getRecipes();
+    }
+
+    onLogout() {
+        this.authService.logout();
     }
 }
