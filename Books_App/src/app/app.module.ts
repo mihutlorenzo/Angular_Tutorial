2393bf80ce1fd09901be354723d8c0a141fe8800
MihutLorenzo <lc.mihut@gmail.com>
2019-02-27 16:42:37 +0200

    Created NgModule for auth components and added lazy loading for recipes components

--- Books_App/src/app/app.module.ts
+++ Books_App/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
@@ -9,28 +8,26 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { RecipeService } from './recipes/recipe.service';
 import { DataStorageService } from './shared/data-storage.service';
-import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component';
 import { AuthService } from './auth/auth.service';
 import { AuthGuard } from './auth/auth-guard.service';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
+import { AuthModule } from './auth/auth.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    SignupComponent,
-    SigninComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    RecipesModule,
     ShoppingListModule,
+    AuthModule,
+    SharedModule,
     AppRoutingModule,
-    HttpModule,
-    SharedModule
+    HttpModule
   ],
   providers: [ShoppingListService,
               RecipeService,
