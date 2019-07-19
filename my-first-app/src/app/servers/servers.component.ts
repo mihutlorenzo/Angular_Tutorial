ab39c4a237182a4c63343d711a2eb325e2488e5a
MihutLorenzo <lc.mihut@gmail.com>
2019-07-19 17:03:46 +0300

    First assigment to do

---
+++ my-first-app/src/app/servers/servers.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  // selector: '[app-servers]',
+  // tslint:disable-next-line:component-selector
+  selector: '.app-servers',
+  template: `
+  <app-server></app-server>
+  <app-server></app-server>`,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
