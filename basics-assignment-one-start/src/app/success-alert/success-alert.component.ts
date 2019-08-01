99184424a2a61266482d363532d2f73b80129314
enzojack <lc.mihut@gmail.com>
2019-08-01 08:34:22 +0300

    Merge branch 'master' of https://github.com/mihutlorenzo/Angular_Tutorial
    # Conflicts:
    #	my-first-app/.gitignore
    #	my-first-app/README.md
    #	my-first-app/angular.json
    #	my-first-app/e2e/protractor.conf.js
    #	my-first-app/e2e/src/app.e2e-spec.ts
    #	my-first-app/e2e/src/app.po.ts
    #	my-first-app/package-lock.json
    #	my-first-app/package.json
    #	my-first-app/src/app/app.component.html
    #	my-first-app/src/app/app.component.ts
    #	my-first-app/src/app/app.module.ts
    #	my-first-app/src/polyfills.ts
    #	my-first-app/tsconfig.json
    #	my-first-app/tslint.json

---
+++ basics-assignment-one-start/src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-success-alert',
+    template: `<p>Success alert done</p>`,
+    styles: [`
+        p {
+            color: red;
+        }
+    `]
+})
+export class SuccessAlertComponent {
+
+}
--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -5,16 +5,36 @@ import { Component, OnInit } from '@angular/core';
   // selector: '[app-servers]',
   // tslint:disable-next-line:component-selector
   selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['Testserver', 'Testserver 2'];
 
-  constructor() { }
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
 
   ngOnInit() {
+  }
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
